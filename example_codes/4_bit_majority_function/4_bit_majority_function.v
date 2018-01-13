c83d0e9225682f0c2242b220d1bf30713889ca4b
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2018-01-13 19:27:02 +0530

    added verilog code for four bit majority function

---
+++ example_codes/4_bit_majority_function/4_bit_majority_function.v
@@ -0,0 +1,54 @@
+primitive four_bit_majority_function(e,a,b,c,d);
+    output e;
+    input a,b,c,d;
+    table
+  //    a    b    c    d    :    e
+	0   0   0   0   :   0;
+	0   0   0   1   :   0;
+	0   0   1   0   :   0;
+	0   0   1   1   :   0;
+	0   1   0   0   :   0;
+	0   1   0   1   :   0;
+	0   1   1   0   :   0;
+	0   1   1   1   :   1;
+	1   0   0   0   :   0;
+	1   0   0   1   :   0;
+	1   0   1   0   :   0;
+	1   0   1   1   :   1;
+	1   1   0   0   :   0;
+	1   1   0   1   :   1;
+	1   1   1   0   :   1;
+	1   1   1   1   :   1;
+    endtable
+endprimitive
+
+module fourbit(e,a,b,c,d);
+    output e;
+    input a,b,c,d;
+    four_bit_majority_function g(e,a,b,c,d);
+endmodule
+
+module test_four_bit_function();
+    wire e;
+    reg a,b,c,d;
+    fourbit G(e,a,b,c,d);
+    initial
+        begin
+	    a=0;b=0;c=0;d=0;
+	#50 a=0;b=0;c=0;d=1;
+	#50 a=0;b=0;c=1;d=0;
+	#50 a=0;b=0;c=1;d=1;
+	#50 a=0;b=1;c=0;d=0;
+	#50 a=0;b=1;c=0;d=1;
+	#50 a=0;b=1;c=1;d=0;
+	#50 a=0;b=1;c=1;d=1;
+	#50 a=1;b=0;c=0;d=0;
+	#50 a=1;b=0;c=0;d=1;
+	#50 a=1;b=0;c=1;d=0;
+	#50 a=1;b=0;c=1;d=1;
+	#50 a=1;b=1;c=0;d=0;
+	#50 a=1;b=1;c=0;d=1;
+	#50 a=1;b=1;c=1;d=0;
+	#50 a=1;b=1;c=1;d=1;
+	end
+endmodule

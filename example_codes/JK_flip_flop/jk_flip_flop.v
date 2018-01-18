fd45c04d82ab75b63b6b6daa52b60d0832f704fb
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2018-01-18 09:47:59 +0530

    updated jk flip flop verilog code

--- example_codes/JK_flip_flop/jk_flip_flop.v
+++ example_codes/JK_flip_flop/jk_flip_flop.v
@@ -3,10 +3,10 @@
 module JK_flip_flop(output reg Q, Q_b, input J, K, clk);
 	always @(posedge clk)
 		case({J,K})
-			{1'b0,1'b0}: begin Q<=Q; Q_b<=Q_b; end
-			{1'b0,1'b1}: begin Q<=0; Q_b<=1; end
-			{1'b1,1'b0}: begin Q<=1; Q_b<=0; end
-			{1'b0,1'b1}: begin Q<=!Q; Q_b<=!Q; end
+			2'b00: begin Q<=Q; Q_b<=Q_b; end
+			2'b01: begin Q<=0; Q_b<=1; end
+			2'b10: begin Q<=1; Q_b<=0; end
+			2'b11: begin Q<=!Q; Q_b<=!Q_b; end
 		endcase
 endmodule
 
@@ -15,13 +15,13 @@ endmodule
 module test_JK_flip_flop();
 	wire Q,Q_b;
 	reg J,K,clk;
-	JK_flip_flop G(Q,Q_b,JK,clk);
+	JK_flip_flop G(Q,Q_b,J,K,clk);
 	initial
 		begin
-		clk=1;J=0;K=1;
+		J=0;K=1;clk=0;
+		#100 clk=1;
 		#100 clk=0;J=0;K=0;
-	    #100 J=1;K=1;
-		#100 J=0;K=1;
+       	        #100 J=1;K=1;
 		#100 clk=1;J=1;K=0;
 		#100 clk=0;
 		#100 J=1;K=1;

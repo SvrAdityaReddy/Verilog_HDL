4bc10a8671397f555b0750869aade18678bdde5d
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2018-01-24 01:09:43 +0530

    verilog code of up counter 3 bit updated

--- example_codes/UP_counter_3_bit/up_counter_3_bit.v
+++ example_codes/UP_counter_3_bit/up_counter_3_bit.v
@@ -17,8 +17,11 @@ endmodule
 //     or G6(Q[2],I2,I3);
 // endmodule
 
-module up_counter_3_bit(output [2:0] Q_b, input [2:0] Q, input clk);
+module up_counter_3_bit(output [2:0] Q_b, input clk);
     wire I1,I2,I3,I4,I5;
+    reg [2:0] Q;
+    initial
+        Q<=3'b000;
     D_flip_flop G1(Q_b[0],~Q[0],clk);
     xor G2(I1,Q[1],Q[0]);
     D_flip_flop G3(Q_b[1],I1,clk);
@@ -27,17 +30,23 @@ module up_counter_3_bit(output [2:0] Q_b, input [2:0] Q, input clk);
     and G6(I4,~Q[2],Q[1],Q[0]);
     or G7(I5,I4,I3);
     D_flip_flop G8(Q_b[2],I5,clk);
+    always @*
+        Q=Q_b;
+    // and G9(Q[0],Q_b[0],1);
+    // and G10(Q[1],Q_b[1],1);
+    // and G11(Q[2],Q_b[2],1);
 endmodule
+
+// test bench code
 
 module test_up_counter_3_bit();
     wire [2:0] Q_b;
-    reg [2:0] Q;
+    // reg [2:0] Q;
     reg clk;
-    up_counter_3_bit G(Q_b,Q,clk);
+    up_counter_3_bit G(Q_b,clk);
     initial
         begin
-            clk=1;Q=3'b010;
-            #50 clk=0;
+            clk=0;
             #50 clk=1;
             #50 clk=0;
             #50 clk=1;
@@ -48,8 +57,10 @@ module test_up_counter_3_bit();
             #50 clk=0;
             #50 clk=1;
             #50 clk=0;
-            // #50 clk=1;
-            // #50 clk=0;
-            // #50 clk=1;
+            #50 clk=1;
+            #50 clk=0;
+            #50 clk=1;
+            #50 clk=0;
+            #50 clk=1;
         end
 endmodule
